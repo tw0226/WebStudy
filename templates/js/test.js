368736d0c210f7ebf2ae27dc5e16ead603e82dc5
taewoo kim <kimraewoo99@gmail.com>
2020-11-04 17:37:08 +0900

    script accept

--- templates/js/test.js
+++ templates/js/test.js
@@ -1,1 +1,23 @@
 var board_column_list = ['id', '게시물명', '작성자']
+
+function load_top_table() {
+  'document.write("<tr>");'
+  for(var i=0;i<board_column_list.length;i++){
+    document.write("<th> "+board_column_list[i]+" </th>");
+  }
+  'document.write("</tr>");'
+}
+
+function make_table(rows, cols){
+  '--! var row = rows; !--'
+  'document.write(rows+", "+cols);'
+  'document.write("<table>");'
+  for(var i=0;i<parseInt(rows); i++){
+    document.write("<tr>");
+    for(var j=0;j<parseInt(cols); j++){
+      document.write("<td> "+i+","+j+" </td>");
+    }
+    document.write("</tr>");
+  }
+  'document.write("</table>");'
+}
